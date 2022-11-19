9ae45e6229a1b51bd4d1aaec90ad0645b71bad87
R4YH4N <112091560+sheikrayhan@users.noreply.github.com>
2022-11-19 23:46:20 +0600

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+$(function () {
+   // Slick Activate
+   $('.bannerSlider').slick({
+      prevArrow: '<i class="fa-solid fa-chevron-left banner_slider_icon"></i>',
+      nextArrow: '<i class="fa-solid fa-chevron-right banner_slider_icon"></i>',
+      autoplay:true,
+  });
+})
